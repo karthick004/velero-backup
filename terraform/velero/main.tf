1c902014925b2807701a9db8c92e8a850692af81
karthick004 <96881243+karthick004@users.noreply.github.com>
2025-04-26 12:44:41 +0530

    Create main.tf

---
+++ terraform/velero/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "k8s-cluster" # Replace with your real cluster name
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_caller_identity" "current" {}
+
+
+locals {
+  oidc_provider_url = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+  oidc_provider_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_provider_url}"
+}
+
+
+resource "aws_iam_role" "velero_irsa_role" {
+  name = "velero-irsa-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = local.oidc_provider_arn
+        }
+        Sid = ""
+      }
+    ]
+  })
+
+  tags = {
+    Environment = "velero"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "velero_irsa_policy_attachment" {
+  role       = aws_iam_role.velero_irsa_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
